fc78638c24897a063e5a592cc5365e2aa1e79467
Douglas Rossignolli <douglas@douglas-mac.local>
2014-06-12 20:56:47 -0300

    fix production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,6 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -44,6 +42,7 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
+  config.assets.compile = true
   config.assets.precompile += %w( *.js *.css )
 
   # Prepend all log lines with the following tags.
